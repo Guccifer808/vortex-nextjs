47f0e3bd12ef478e02f00ae4d08a973588a355de
Guccifer808 <hucciallstars@gmail.com>
2024-01-03 21:07:22 +0200

    Added /constants. Added Mobile menu. Added media queries and some minor CSS edits

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,31 +1,47 @@
+'use client';
+
+import { linksArray } from '@/constants/navLinks';
 import styles from './links.module.css';
 import NavLink from './nav-link/NavLink';
-import { LinkType } from '@/types/linkTypes';
+import { useState } from 'react';
 
 const Links = () => {
-  const linksArray: LinkType[] = [
-    {
-      title: 'Homepage',
-      path: '/',
-    },
-    {
-      title: 'About',
-      path: '/about',
-    },
-    {
-      title: 'Contact',
-      path: '/contact',
-    },
-    {
-      title: 'Blog',
-      path: '/blog',
-    },
-  ];
+  const [open, setOpen] = useState<boolean>(false);
+
+  const isLoggedIn: boolean = true;
+  const isAdmin: boolean = true;
+
   return (
-    <div className={styles.links}>
-      {linksArray.map((link) => (
-        <NavLink link={link} key={link.title} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {linksArray.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+        {isLoggedIn ? (
+          <>
+            {isAdmin && <NavLink link={{ title: 'Admin', path: '/admin' }} />}
+            <button className={styles.logoutBtn}>Logout</button>
+          </>
+        ) : (
+          <NavLink link={{ title: 'Login', path: '/login' }} />
+        )}
+      </div>
+      <button
+        className={styles.menuBtn}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        // <div className={styles.mobLinks}>
+        <div
+          className={`${styles.mobLinks} ${open ? styles.mobLinksVisible : ''}`}
+        >
+          {linksArray.map((link) => (
+            <NavLink link={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,20 @@
+import { LinkType } from '@/types/linkTypes';
+
+export const linksArray: LinkType[] = [
+  {
+    title: 'Homepage',
+    path: '/',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+  },
+  {
+    title: 'Blog',
+    path: '/blog',
+  },
+];
