6eff53a03b7a4ad3aec6b3847ac44c5e78698692
Guccifer808 <hucciallstars@gmail.com>
2024-01-07 20:27:42 +0200

    Improved data.ts. Added getPosts, getUsers, getUserById

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,1 +1,37 @@
 // data fetch goes here
+
+import { connectToDb } from './db';
+import { Post, User } from './models';
+
+//get all posts
+export const getPosts = async (slug: string) => {
+  try {
+    connectToDb();
+    const posts = await Post.find({ slug });
+    return posts;
+  } catch (error) {
+    throw new Error('Error fetching posts');
+  }
+};
+
+//get all users
+export const getUsers = async () => {
+  try {
+    connectToDb();
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    throw new Error('Error fetching users');
+  }
+};
+
+//get user by Id
+export const getUserById = async (id: string) => {
+  try {
+    connectToDb();
+    const user = await User.findById(id);
+    return user;
+  } catch (error) {
+    throw new Error('Error fetching user');
+  }
+};
